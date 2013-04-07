deedbae1372aff6e423eb487fa18431e76a1a340
l3l0 <leszek.prabucki@gmail.com>
2013-04-07 21:55:09 +0200

    Generate snippets

--- features/Context/FeatureContext.php
+++ features/Context/FeatureContext.php
@@ -37,4 +37,60 @@ class FeatureContext extends MinkContext
     {
         throw new PendingException();
     }
+
+    /**
+     * @Given /^że jestem na stronie produktów$/
+     */
+    public function zeJestemNaStronieProduktow()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^nie jestem zalogowany$/
+     */
+    public function nieJestemZalogowany()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^wypełniam pole "([^"]*)" wartością "([^"]*)"$/
+     */
+    public function wypelniamPoleWartoscia($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^wysyłam formularz$/
+     */
+    public function wysylamFormularz()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^powinna pojawić się lista produktów które moge edytować$/
+     */
+    public function powinnaPojawicSieListaProduktowKtoreMogeEdytowac()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^nie powinna pojawić się lista produktów które moge edytować$/
+     */
+    public function niePowinnaPojawicSieListaProduktowKtoreMogeEdytowac()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^powinen pojawić się błąd "([^"]*)"$/
+     */
+    public function powinenPojawicSieBlad($arg1)
+    {
+        throw new PendingException();
+    }
 }
