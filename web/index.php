593ec11c35c1f35f32ce030cc062b5665a8c3431
l3l0 <leszek.prabucki@gmail.com>
2013-04-08 19:57:28 +0200

    Refactoring use security silex provider and session provider

--- web/index.php
+++ web/index.php
@@ -1,36 +1,50 @@
 <?php
 
-session_start();
-
+use Silex\Provider\SecurityServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
+use Silex\Provider\SessionServiceProvider;
 
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/admin/produkty', function() use($app) {
-    if (isset($_SESSION['user']) && $_SESSION['user']) {
-        return 'Lista produktów';
+$app->register(new SessionServiceProvider);
+$app->register(
+    new SecurityServiceProvider,
+    [
+        'security.firewalls' => [
+            'admin' => [
+                'pattern' => '^/admin/',
+                'logout' => ['logout_path' => '/admin/logout'],
+                'form' => ['login_path' => '/login', 'check_path' => '/admin/login_check'],
+                'users' =>[
+                    'Admin' => ['ROLE_ADMIN', 'QtHIJZSatQY53Uvsf7Vg/QD96IRZWGweEIzjietoM2pttqCC3zcoct14ZSzumiNqplauV9REppaKUjz5QYXOdA==']
+                ]
+            ]
+        ]             
+    ]
+);
+
+$app->get('/login', function(Request $request) use($app) {
+    $error = $app['security.last_error']($request);
+
+    $response = '';
+    if ($error) {
+        $response = '<div class="error">Podałeś zły login lub hasło</div>';
     }
-    if (!isset($_SESSION['user'])) {
-        return <<<LOGIN
-<form method="POST" action="/admin/produkty">
+    $response .= <<<LOGIN
+<form method="POST" action="/admin/login_check">
     <input placeholder="Login" type="text" name="_username"  />
     <input placeholder="Hasło" type="password" name="_password"  />
     <input type="submit" value="Zaloguj" />
 </form>
 LOGIN;
 
-    }
-}); 
-$app->post('/admin/produkty', function(Request $request) use($app) {
-    if ('Admin' === $request->request->get('_username', '')  && 'Foo' === $request->request->get('_password', '')) {
-        $_SESSION['user'] = true;
-        return $app->redirect('/admin/produkty');
-    }
-
-    return '<div class="error">Podałeś zły login lub hasło</div>';
+    return $response;
+});
+$app->get('/admin/produkty', function() use($app) {
+    return 'Lista produktów';
 }); 
 
 $app->run(); 
