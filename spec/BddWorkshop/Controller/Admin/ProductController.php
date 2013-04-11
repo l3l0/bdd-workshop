8c117c67db209d8046d502825c37b70bd0870f57
l3l0 <leszek.prabucki@gmail.com>
2013-04-11 18:50:18 +0200

    Add product add specyfication without implementation.

--- spec/BddWorkshop/Controller/Admin/ProductController.php
+++ spec/BddWorkshop/Controller/Admin/ProductController.php
@@ -33,4 +33,39 @@ class ProductController extends ObjectBehavior
 
         $this->listAction($request, $application)->shouldBe($response);
     }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @param \Silex\Application $application
+     * @param \Twig_Extension $twig
+     */
+    function its_new_action_renders_new_product_template($request, $response, $application, $twig)
+    {
+        $request->isMethod('post')->willReturn(false);
+        $twig->render('admin/productNew.twig')->shouldBeCalled()->willReturn($response);
+
+        $this->newAction($request, $application)->shouldBe($response);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @param \Silex\Application $application
+     * @param \Doctrine\DBAL\Connection $connection
+     * @param \Symfony\Component\HttpFoundation\ParameterBag $postParams
+     */
+    function its_new_action_save_product_and_redirect_to_list_when_request_is_post_and_name_is_not_empty($request, $response, $application, $connection, $postParams)
+    {
+        $request->isMethod('post')->willReturn(true);
+        $request->request = $postParams;
+        $postParams->get('name')->willReturn('Jakas nazwa');
+        $postParams->get('opis')->willReturn('Opis');
+        $postParams->get('cena')->willReturn('123.12');
+
+        $connection->insert('product', ['name' => 'Jakas nazwa', 'opis' => 'Opis', 'cena' => '123.12'])->shouldBeCalled();
+        $application->redirect('/admin/produkty')->shouldBeCalled();
+
+        $this->newAction($request, $application);
+    }
 }
